1069051c1d4af7eb145c4c2deedbde1f33df6c60
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-11-10 00:10:59 +0100

    contact list

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,10 +1,10 @@
-import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-    selectContacts,
-    selectError,
-    selectFilteredContacts,
-    selectLoading,
+  selectContacts,
+  selectError,
+  selectFilteredContacts,
+  selectLoading,
 } from 'redux/contacts/selectors';
 import { deleteContact } from 'redux/contacts/thunks';
 import { Loader } from 'components/Loader/Loader';
@@ -13,107 +13,108 @@ import { Notification } from 'components/Notification/Notification';
 import { selectFilter } from 'redux/filter/selectors';
 import { NotFoundNotification } from 'components/Notification/NotFoundNotification';
 import {
-    Avatar,
-    Box,
-    Card,
-    IconButton,
-    Stack,
-    Typography,
+  Avatar,
+  Box,
+  Card,
+  IconButton,
+  Stack,
+  Typography,
 } from '@mui/material';
-import { deepPurple } from '@mui/material/colors';
+// import { cyan } from '@mui/material/colors';
+import { teal } from '@mui/material/colors';
 import toast from 'react-hot-toast';
 
 export const ContactsList = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const contacts = useSelector(selectContacts);
-    const visibleContacts = useSelector(selectFilteredContacts);
-    const error = useSelector(selectError);
-    const isLoading = useSelector(selectLoading);
-    const filter = useSelector(selectFilter);
+  const contacts = useSelector(selectContacts);
+  const visibleContacts = useSelector(selectFilteredContacts);
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectLoading);
+  const filter = useSelector(selectFilter);
 
-    const handleDeleteContact = async (id, name) => {
-        try {
-            await dispatch(deleteContact(id)).unwrap();
-            toast.success(`${name} deleted!`);
-        } catch (error) {
-            toast.error(`Something went wrong. Error: ${error}.`);
-        }
-    };
+  const handleDeleteContact = async (id, name) => {
+    try {
+      await dispatch(deleteContact(id)).unwrap();
+      toast.success(`${name} deleted!`);
+    } catch (error) {
+      toast.error(`Something went wrong. Error: ${error}.`);
+    }
+  };
 
-    return (
-        <Box
-            sx={{
-                width: '100%',
-                borderTop: '1px solid black',
-                padding: '40px 0',
-            }}
-        >
-            <Typography
-                variant="h5"
-                mb={'20px'}
-                sx={{ textAlign: 'center', fontWeight: '600' }}
-            >
-                Contacts
-            </Typography>
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        borderTop: '1px solid black',
+        padding: '40px 0',
+      }}
+    >
+      <Typography
+        variant="h5"
+        mb={'20px'}
+        sx={{ textAlign: 'center', fontWeight: '600' }}
+      >
+        Contacts
+      </Typography>
 
-            {isLoading && <Loader />}
+      {isLoading && <Loader />}
 
-            {!isLoading && visibleContacts.length > 0 && (
-                <Stack spacing={2}>
-                    {visibleContacts.map(({ id, name, number }) => {
-                        return (
-                            <Card
-                                key={id}
-                                sx={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'space-between',
-                                }}
-                            >
-                                <Avatar
-                                    sx={{
-                                        bgcolor: deepPurple[500],
-                                    }}
-                                >
-                                    {name[0]}
-                                </Avatar>
-                                <Typography variant="body">
-                                    {name}: {number}
-                                </Typography>
-                                <IconButton
-                                    aria-label="delete"
-                                    size="large"
-                                    color="error"
-                                    onClick={() => {
-                                        handleDeleteContact(id, name);
-                                    }}
-                                >
-                                    <PersonRemoveIcon fontSize="inherit" />
-                                </IconButton>
-                            </Card>
-                        );
-                    })}
-                </Stack>
-            )}
+      {!isLoading && visibleContacts.length > 0 && (
+        <Stack spacing={2}>
+          {visibleContacts.map(({ id, name, number }) => {
+            return (
+              <Card
+                key={id}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Avatar
+                  sx={{
+                    bgcolor: teal[300],
+                  }}
+                >
+                  {name?.at(0)?.toUpperCase()}
+                </Avatar>
+                <Typography variant="body">
+                  {name}: {number}
+                </Typography>
+                <IconButton
+                  aria-label="delete"
+                  size="large"
+                  color="default"
+                  onClick={() => {
+                    handleDeleteContact(id, name);
+                  }}
+                >
+                  <DeleteTwoToneIcon fontSize="inherit" />
+                </IconButton>
+              </Card>
+            );
+          })}
+        </Stack>
+      )}
 
-            {!error && !isLoading && contacts.length === 0 && (
-                <Notification>Your phone book is empty!</Notification>
-            )}
+      {!error && !isLoading && contacts.length === 0 && (
+        <Notification>Your contact list is empty!</Notification>
+      )}
 
-            {/* {error && !isLoading && (
+      {/* {error && !isLoading && (
                 <ErrorNotification>
                     Oops... Something went wrong. Error: {error}. Please, try
                     again.
                 </ErrorNotification>
             )} */}
 
-            {filter && !isLoading && visibleContacts.length === 0 && (
-                <NotFoundNotification>
-                    No contacts matching your search query: "{filter}". Please
-                    change your search parameters and try again.
-                </NotFoundNotification>
-            )}
-        </Box>
-    );
+      {filter && !isLoading && visibleContacts.length === 0 && (
+        <NotFoundNotification>
+          No contacts matching your search query: "{filter}". Please change your
+          search parameters and try again.
+        </NotFoundNotification>
+      )}
+    </Box>
+  );
 };
