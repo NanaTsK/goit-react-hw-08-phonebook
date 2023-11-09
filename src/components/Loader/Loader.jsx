3c0f15a856b3664cefdac4673f3741780166b4fe
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-11-09 22:12:12 +0100

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,18 @@
-import { RotatingLines } from 'react-loader-spinner';
-import { SpinnerLoad } from './Loader.styled';
+import { ColorRing } from 'react-loader-spinner';
+import { LoaderWrap } from './Loader.styled';
 
 export const Loader = () => {
-    return (
-        <SpinnerLoad>
-            <RotatingLines
-                strokeColor="#424D55"
-                strokeWidth="5"
-                animationDuration="0.75"
-                width="96"
-                visible={true}
-            />
-        </SpinnerLoad>
-    );
+  return (
+    <LoaderWrap>
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
+    </LoaderWrap>
+  );
 };
--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -1,8 +1,8 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
-export const SpinnerLoad = styled('div')`
-    display: flex;
-    margin-top: 40px;
-    justify-content: center;
-    opacity: 0.6;
+export const LoaderWrap = styled.div`
+  display: flex;
+  margin-top: 40px;
+  justify-content: center;
+  opacity: 0.6;
 `;
