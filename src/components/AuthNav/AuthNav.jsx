ae40713188a644e1be502a9929f2b2046f7f5633
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-11-09 18:40:21 +0100

    start

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -3,42 +3,43 @@ import { UserMenu } from 'components/UserMenu/UserMenu';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { selectLoggedIn } from 'redux/auth/selectors';
+// import { selectLoggedIn } from 'redux/auth/selectors';
 
 export const AuthNav = () => {
-    const isLoggedIn = useSelector(selectLoggedIn);
+  const isLoggedIn = useSelector(selectLoggedIn);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return isLoggedIn ? (
-        <UserMenu />
-    ) : (
-        <Box
-            sx={{
-                flexGrow: 1,
-                display: { xs: 'none', md: 'flex' },
-            }}
-        >
-            <Button
-                onClick={() => navigate('login')}
-                sx={{
-                    my: 2,
-                    color: 'white',
-                    display: 'block',
-                }}
-            >
-                Login
-            </Button>
+  return isLoggedIn ? (
+    <UserMenu />
+  ) : (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: { xs: 'none', md: 'flex' },
+      }}
+    >
+      <Button
+        onClick={() => navigate('login')}
+        sx={{
+          my: 2,
+          color: 'white',
+          display: 'block',
+        }}
+      >
+        Login
+      </Button>
 
-            <Button
-                onClick={() => navigate('register')}
-                sx={{
-                    my: 2,
-                    color: 'white',
-                    display: 'block',
-                }}
-            >
-                Sign up
-            </Button>
-        </Box>
-    );
+      <Button
+        onClick={() => navigate('register')}
+        sx={{
+          my: 2,
+          color: 'white',
+          display: 'block',
+        }}
+      >
+        Sign up
+      </Button>
+    </Box>
+  );
 };
