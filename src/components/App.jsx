98d9f909698fb9a4db5556dcb69e4e896ae98573
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-11-09 18:32:03 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,72 @@
-import { Container } from './index.styled';
-import { Loader } from './Loader/Loader';
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from './Layout/SharedLayout';
+
+import { lazy, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectAppState } from 'redux/app/selectors';
-import { useEffect } from 'react';
-import { getAllContactsThunk } from 'redux/contact/thunks';
+import { refresh } from 'redux/auth/thunks';
+import { RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute/PrivateRoute';
+import { selectRefreshing } from 'redux/auth/selectors';
+import { Toaster } from 'react-hot-toast';
 
-const App = () => {
-  const { isLoading, error } = useSelector(selectAppState);
+const Home = lazy(() => import('pages/Home'));
+const Contacts = lazy(() => import('pages/Contacts'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Register = lazy(() => import('pages/Register'));
+const Login = lazy(() => import('pages/Login'));
+const Profile = lazy(() => import('pages/Profile'));
 
-  const dispatch = useDispatch();
+export const App = () => {
+    const dispatch = useDispatch();
+    const isRefreshing = useSelector(selectRefreshing);
 
-  useEffect(() => {
-    dispatch(getAllContactsThunk());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(refresh());
+    }, [dispatch]);
 
-  return (
-    <Container>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      {isLoading && <Loader />}
-      {error && <h1>Oooops! Something went wrong</h1>}
-      <ContactList />
-    </Container>
-  );
+    return (
+        <>
+            <Toaster />
+            {!isRefreshing && (
+                <Routes>
+                    <Route path="/" element={<SharedLayout />}>
+                        <Route index element={<Home />} />
+                        <Route
+                            path="register"
+                            element={
+                                <RestrictedRoute>
+                                    <Register />
+                                </RestrictedRoute>
+                            }
+                        />
+                        <Route
+                            path="login"
+                            element={
+                                <RestrictedRoute>
+                                    <Login />
+                                </RestrictedRoute>
+                            }
+                        />
+                        <Route
+                            path="contacts"
+                            element={
+                                <PrivateRoute>
+                                    <Contacts />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path="profile"
+                            element={
+                                <PrivateRoute>
+                                    <Profile />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                </Routes>
+            )}
+        </>
+    );
 };
-
-export default App;
--- src/components/Filter/Filter.styled.js
+++
@@ -1,7 +0,0 @@
-// import styled from 'styled-components';
-
-// export const FilterForm = styled('form')({
-//   display: 'flex',
-//   flexDirection: 'column-reverse',
-//   paddingBottom: '15px',
-// });
