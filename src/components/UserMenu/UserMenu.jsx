a14a12e38c771016008d1e9c65e2957a4bcfdcf1
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-11-10 02:16:53 +0100

    profile

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,13 +1,13 @@
 import {
-    Avatar,
-    Box,
-    IconButton,
-    Menu,
-    MenuItem,
-    Tooltip,
-    Typography,
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
 } from '@mui/material';
-import { deepPurple } from '@mui/material/colors';
+import { teal } from '@mui/material/colors';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -15,61 +15,67 @@ import { selectUserName } from 'redux/auth/selectors';
 import { logOut } from 'redux/auth/thunks';
 
 export const UserMenu = () => {
-    const [anchorElUser, setAnchorElUser] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const name = useSelector(selectUserName);
+  const name = useSelector(selectUserName);
 
-    const handleLogout = () => {
-        handleCloseUserMenu();
-        dispatch(logOut());
-    };
+  const handleLogout = () => {
+    handleCloseUserMenu();
+    dispatch(logOut());
+  };
 
-    const handleNavigateToProfile = () => {
-        handleCloseUserMenu();
-        navigate('profile');
-    };
+  const handleNavigateToProfile = () => {
+    handleCloseUserMenu();
+    navigate('profile');
+  };
 
-    const handleOpenUserMenu = event => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenUserMenu = event => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    return (
-        <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar sx={{ bgcolor: deepPurple[500] }}>{name[0]}</Avatar>
-                </IconButton>
-            </Tooltip>
-            <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-            >
-                <MenuItem onClick={handleNavigateToProfile}>
-                    <Typography textAlign="center">Profile</Typography>
-                </MenuItem>
-                <MenuItem onClick={handleLogout}>
-                    <Typography textAlign="center">Logout</Typography>
-                </MenuItem>
-            </Menu>
-        </Box>
-    );
+  return (
+    <Box sx={{ flexGrow: 0 }}>
+      <Tooltip title="Open settings">
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar
+            sx={{
+              bgcolor: teal[300],
+            }}
+          >
+            {name?.at(0)?.toUpperCase()}
+          </Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: '45px' }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        <MenuItem onClick={handleNavigateToProfile}>
+          <Typography textAlign="center">Profile</Typography>
+        </MenuItem>
+        <MenuItem onClick={handleLogout}>
+          <Typography textAlign="center">Logout</Typography>
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
 };
--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,51 +1,54 @@
 import {
-    Avatar,
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    Typography,
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
 } from '@mui/material';
-import { deepPurple } from '@mui/material/colors';
+import { teal } from '@mui/material/colors';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUserEmail, selectUserName } from 'redux/auth/selectors';
 import { logOut } from 'redux/auth/thunks';
 
 export const UserProfile = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const name = useSelector(selectUserName);
-    const email = useSelector(selectUserEmail);
+  const name = useSelector(selectUserName);
+  const email = useSelector(selectUserEmail);
 
-    return (
-        <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-                <Avatar
-                    sx={{
-                        bgcolor: deepPurple[500],
-                        marginBottom: '16px',
-                        width: 60,
-                        height: 60,
-                    }}
-                >
-                    {name[0]}
-                </Avatar>
-                <Typography variant="h5">
-                    <b>Name:</b> {name}
-                </Typography>
-                <Typography variant="h5">
-                    <b>Email:</b> {email}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button
-                    size="large"
-                    variant="contained"
-                    onClick={() => dispatch(logOut())}
-                >
-                    Logout
-                </Button>
-            </CardActions>
-        </Card>
-    );
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Avatar
+          sx={{
+            bgcolor: teal[300],
+            marginBottom: '16px',
+            width: 60,
+            height: 60,
+          }}
+        >
+          {name?.at(0)?.toUpperCase()}
+        </Avatar>
+        <Typography variant="h5">
+          <b>Name:</b> {name}
+        </Typography>
+        <Typography variant="h5">
+          <b>Email:</b> {email}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          size="large"
+          variant="contained"
+          sx={{
+            bgcolor: teal[700],
+          }}
+          onClick={() => dispatch(logOut())}
+        >
+          Logout
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
