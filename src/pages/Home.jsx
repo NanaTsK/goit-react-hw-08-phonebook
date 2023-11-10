61479e3511835df4fa632fc94806e9590962ac29
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-11-10 02:16:42 +0100

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,39 +4,38 @@ import { Link } from 'react-router-dom';
 import { selectLoggedIn, selectUserName } from 'redux/auth/selectors';
 
 const Home = () => {
-    const isLoggedIn = useSelector(selectLoggedIn);
-    const name = useSelector(selectUserName);
+  const isLoggedIn = useSelector(selectLoggedIn);
+  const name = useSelector(selectUserName);
 
-    return (
-        <section>
-            <Typography
-                variant="h2"
-                mb={'20px'}
-                sx={{ textAlign: 'center', fontWeight: '600' }}
-            >
-                Phonebook app.
-            </Typography>
-            {isLoggedIn ? (
-                <Typography
-                    variant="h4"
-                    mb={'20px'}
-                    sx={{ textAlign: 'center', fontWeight: '600' }}
-                >
-                    Welcome {name}! Want to go to the{' '}
-                    <Link to="contacts">contacts</Link>?
-                </Typography>
-            ) : (
-                <Typography
-                    variant="h4"
-                    mb={'20px'}
-                    sx={{ textAlign: 'center', fontWeight: '600' }}
-                >
-                    Please <Link to="login">login</Link> to your account or{' '}
-                    <Link to="register">sign up</Link>!
-                </Typography>
-            )}
-        </section>
-    );
+  return (
+    <section>
+      <Typography
+        variant="h3"
+        mb={'20px'}
+        sx={{ textAlign: 'center', fontWeight: '600', opacity: '0.4' }}
+      >
+        PhoneBook App.
+      </Typography>
+      {isLoggedIn ? (
+        <Typography
+          variant="h4"
+          mb={'20px'}
+          sx={{ textAlign: 'center', fontWeight: '600', color: '#4db6ac' }}
+        >
+          Welcome {name}! <br /> Visit your <Link to="contacts">contacts</Link>!
+        </Typography>
+      ) : (
+        <Typography
+          variant="h4"
+          mb={'20px'}
+          sx={{ textAlign: 'center', fontWeight: '600', color: '#4db6ac' }}
+        >
+          All your contacts at one place. <br />
+          <Link to="login">Login</Link> | <Link to="register">Sign up</Link>
+        </Typography>
+      )}
+    </section>
+  );
 };
 
 export default Home;
