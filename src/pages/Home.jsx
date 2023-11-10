33c68834b7396f8d038e09fb1629620caa1902b0
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-11-10 10:33:37 +0100

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import ContactPhoneTwoToneIcon from '@mui/icons-material/ContactPhoneTwoTone';
+
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { selectLoggedIn, selectUserName } from 'redux/auth/selectors';
@@ -9,24 +11,50 @@ const Home = () => {
 
   return (
     <section>
-      <Typography
-        variant="h3"
-        mb={'20px'}
-        sx={{ textAlign: 'center', fontWeight: '600', opacity: '0.4' }}
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: { xs: 'none', md: 'flex' },
+          alignItems: 'center',
+          justifyContent: 'center',
+          opacity: '0.6',
+          mb: '20px',
+        }}
       >
-        PhoneBook App.
-      </Typography>
+        <ContactPhoneTwoToneIcon
+          sx={{ display: { xs: 'none', md: 'flex' }, mr: 1, marginTop: '-3px' }}
+        />
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="/goit-react-hw-08-phonebook"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            marginBottom: 0,
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          PhoneBook
+        </Typography>
+      </Box>
+
       {isLoggedIn ? (
         <Typography
-          variant="h4"
+          variant="h5"
           mb={'20px'}
           sx={{ textAlign: 'center', fontWeight: '600', color: '#4db6ac' }}
         >
-          Welcome {name}! <br /> Visit your <Link to="contacts">contacts</Link>!
+          Welcome {name}! <br /> Visit your <Link to="contacts">Contacts</Link>!
         </Typography>
       ) : (
         <Typography
-          variant="h4"
+          variant="h5"
           mb={'20px'}
           sx={{ textAlign: 'center', fontWeight: '600', color: '#4db6ac' }}
         >
