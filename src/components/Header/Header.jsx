1b9bee669e4ff80738e64a286e012452625e123e
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-11-10 02:16:36 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,22 @@
 import { AppBar, Container, Toolbar } from '@mui/material';
 import { AuthNav } from 'components/AuthNav/AuthNav';
 import { Navigation } from 'components/Navigation/Navigation';
+import { teal } from '@mui/material/colors';
 
 export const Header = () => {
-    return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Navigation />
-                    <AuthNav />
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        bgcolor: teal[700],
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Navigation />
+          <AuthNav />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
